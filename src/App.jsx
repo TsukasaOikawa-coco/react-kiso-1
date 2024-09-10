722335efc93c47a23a20e72e8cf618313d19b8d4
TsukasaOikawa-coco <coco1207t@gmail.com>
2024-09-10 21:47:06 +0900

    2024/09/10

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { useState,useEffect } from 'react';
 import './App.css';
 
-function App() {
-    return (
-        <div>
-            <h1>こん</h1>
-        </div>
-    );
-}
-
-export default App;
+export const App = () => {
+  const [threads, SetThreads] = useState([]);
+  
+  useEffect(() => {
+    fetch('https://railway.bulletinboard.techtrain.dev/thread')
+      .then(res => res.json())
+      .then(data => SetThreads(data.message)) 
+  },[])
+  return(
+  <div>
+    <header>新着スレッド</header>
+    <ul>
+        {threads.map((thread, index) => (
+          <li key={index}> {threads} </li>
+        ))}
+    </ul>
+  </div>
+  );
+};
