c5285afeaee8b27c18f16912450499c45f43e008
TsukasaOikawa-coco <coco1207t@gmail.com>
2024-09-13 04:40:55 +0900

    station3

--- src/component/CreateThreads.jsx
+++ src/component/CreateThreads.jsx
@@ -6,6 +6,10 @@ export const CreateThreads = () => {
   return(
   <div>
     <h1>スレッド新規作成</h1>
+    <form action='https://railway.bulletinboard.techtrain.dev' method='post'>
+      <input type='text' placeholder='スレッドタイトル'></input>
+      <input type='submit' value='作成'></input>
+    </form>
     <Link to="/">home</Link>
   </div>
   )
